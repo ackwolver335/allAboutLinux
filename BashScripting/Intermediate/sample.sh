7006773c14e1f85edef409bfd458a1a75fd0c358
ackwolver335 <abhaych335@gmail.com>
2025-10-28 23:07:35 +0530

    Bash Scripting (Intermediate) - Advanced Bash

--- BashScripting/Intermediate/sample.sh
+++ BashScripting/Intermediate/sample.sh
@@ -2,10 +2,29 @@
 
 ps -l
 
-exec N<>sample.sh
+# exec N<>sample.sh
 
 # closing the file descriptor
-exec N<&-
+# exec N<&-
 
 # for listing the open files
-lsof
+# lsof
+
+<<COMMENT
+while read line
+do
+    echo $line | tr "ABC" "abc"                     # will translate every ABC to abc wherever it is written
+done
+COMMENT
+
+trap '' INT                                         # this will be an infinite loop when someone try to stop the terminal
+trap 'echo stopping..; exit' INT
+trap "echo you cannot quit now" QUIT
+cd /
+while true
+do
+    echo looping...
+    du -m * 2>/dev/null
+    echo sleeping..
+    sleep 5
+done
